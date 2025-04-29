c72108b028fcee8dfb8b56693361d155dad34392
beu2901 <beu2901@gmail.com>
2025-04-29 09:42:29 +0700

    modify card class

--- src/java/model/Card.java
+++ src/java/model/Card.java
@@ -24,4 +24,33 @@ public class Card {
         this.value = value;
     }
 
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
 }
