a1c7b96544575fd2220559fe205282a990831db8
beu2901 <beu2901@gmail.com>
2025-04-30 22:02:46 +0700

    create deck constructor

--- src/java/model/Deck.java
+++ src/java/model/Deck.java
@@ -5,10 +5,38 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author beu29
  */
 public class Deck {
-    
+
+    private List<Card> cards;
+
+    private void initialize() {
+        String suits[] = {"Spades", "Clubs", "Diamonds", "Hearts"};
+        String ranks[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        int values[] = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+
+        for (String suit : suits) {
+            for (int i = 0; i < ranks.length; i++) {
+                cards.add(new Card(suit, ranks[i], values[i]));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Deck() {
+        cards = new ArrayList<>();
+        initialize();
+        shuffle();
+    }
+
 }
