669975fe0c5350d6ce7d23bef1d501acfe25d194
beu2901 <beu2901@gmail.com>
2025-04-30 22:05:51 +0700

    create hand class

--- src/java/model/Hand.java
+++ src/java/model/Hand.java
@@ -5,10 +5,27 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author beu29
  */
 public class Hand {
-    
+
+    private List<Card> cards;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
 }
